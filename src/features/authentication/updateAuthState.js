0af2c9b4e0222cd29968d7f72475ea0f41a503fa
César David Parada Castillo <cesard.0925@gmail.com>
2023-06-09 16:14:59 +0200

    feat: añadidos authSlice  y authState

---
+++ src/features/authentication/updateAuthState.js
@@ -0,0 +1,33 @@
+import { decodeToken } from "react-jwt";
+import { store } from "../../app/store";
+import { setIsLoggedIn, setToken, setUserInfo } from "./authSlice";
+
+// Loggin
+export const updateAuthStoreStateLogIn = (token) => {
+  const myDecodedToken = decodeToken(token);
+
+  // dispatch
+  store.dispatch(setIsLoggedIn(true));
+  store.dispatch(
+    setUserInfo({
+      id: myDecodedToken.usuario_id,
+      name: myDecodedToken.usuario_name,
+      role: myDecodedToken.usuario_role,
+    })
+  );
+  store.dispatch(setToken(token));
+};
+
+// Loggout
+export const updateAuthStoreStateLogOut = () => {
+  // dispatch
+  store.dispatch(setIsLoggedIn(false));
+  store.dispatch(
+    setUserInfo({
+      id: "",
+      name: "",
+      role: "",
+    })
+  );
+  store.dispatch(setToken(""));
+};
